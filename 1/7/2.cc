b58f1696b3bc59af37d52f0b6a153f1683acefa4
KujouRinka <kujourinka@gmail.com>
2026-05-09 20:16:27 +0900

    1.7.2

---
+++ 1/7/2.cc
@@ -0,0 +1,139 @@
+#include <iostream>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#include "shader.hh"
+
+GLfloat vertices[] = {
+  0.5f, 0.5f, 0.0f, 1.0f, 1.0f,   // right up,
+  0.5f, -0.5f, 0.0f, 1.0f, 0.0f,  // right down,
+  -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, // left down,
+  -0.5f, 0.5f, 0.0f, 0.0f, 1.0f,  // left up,
+};
+
+GLuint indices[] = {
+  0, 1, 2,
+  0, 2, 3,
+};
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+void process_input(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
+}
+
+int main() {
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+  GLFWwindow *window =
+    glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+  if (window == nullptr) {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+
+  Shader shader("./vert2.vert", "./frag2.frag");
+
+  GLuint texture0;
+  glGenTextures(1, &texture0);
+  glActiveTexture(GL_TEXTURE0);
+  glBindTexture(GL_TEXTURE_2D, texture0);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  int height, width, nr_channel;
+  unsigned char *data = stbi_load("./assets/container.jpg", &width, &height, &nr_channel, 0);
+  if (!data) {
+    std::cout << "Failed to load texture0" << std::endl;
+    return -1;
+  }
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+  glGenerateMipmap(GL_TEXTURE_2D);
+  stbi_image_free(data);
+
+  GLuint VAO, VBO, EBO;
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glBindVertexArray(VAO);
+  // loc
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 5, (void *) 0);
+  // tex coord
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 5, (void *) (sizeof(GLfloat) * 3));
+  glEnableVertexAttribArray(0);
+  glEnableVertexAttribArray(1);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  glm::mat4 trans{1.0f};
+  glm::vec3 vec{0.4f, 0.3f, 0.0f};
+  // move
+  trans = glm::translate(trans, vec);
+  // scale
+  trans = glm::scale(trans, glm::vec3{0.5f, 0.5f, 0.0f});
+
+  glm::mat4 trans2{1.0f};
+  trans2 = glm::translate(trans2, glm::vec3{-0.3f, 0.3f, 0.0f});
+
+  while (!glfwWindowShouldClose(window)) {
+    process_input(window);
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    shader.use();
+    shader.setInt("texture0", 0);
+    // rotate
+    trans = glm::rotate(trans, glm::radians(1.0f), glm::vec3{0.0f, 0.0f, 1.0f});
+    glUniformMatrix4fv(glGetUniformLocation(shader.getHandler(), "transform"), 1, GL_FALSE, glm::value_ptr(trans));
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void *) 0);
+
+    float factor = glm::abs(glm::sin(glfwGetTime()));
+    glm::mat4 trans3 = glm::scale(trans2, glm::vec3{factor, factor, 0.0f});
+    glUniformMatrix4fv(glGetUniformLocation(shader.getHandler(), "transform"), 1, GL_FALSE, glm::value_ptr(trans3));
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void *) 0);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+
+  return 0;
+}
